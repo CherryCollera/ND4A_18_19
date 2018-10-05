dd862b548e83587628591b260e8346c6ffca4563
vinceljuniond4a <43748714+vinceljuniond4a@users.noreply.github.com>
2018-10-05 19:15:41 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConstructorOverloding_PangilinanJunio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            sample s1 = new sample();
+            sample s = new sample(" Pangilinan, Reineth ", " Junio, Mary joyce vincel ");
+            Console.WriteLine("Constructors Overloading");
+            Console.WriteLine(s.firstname + "," + s.secondname);
+            Console.WriteLine("\n" + s1.firstname + "\n\n" + s1.secondname);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ sample.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConstructorOverloding_PangilinanJunio
+{
+    class sample
+    {
+
+     public string firstname, secondname;
+        public sample()
+        {
+            firstname = "Pangilinan , Reineth";
+            secondname = "Junio , Mary Joyce Vincel";
+        }
+
+        public sample(string r, string v)
+        {
+
+            firstname = r;
+            secondname = v;
+        }
+    }
+}
